e2609ed0518b99cec7c0eca1694e4667d2c62111
Adnan-961 <adnan.sayed.official@gmail.com>
2022-09-02 16:19:54 +0300

    progress

---
+++ edu/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss"
+import Navbar from "./Sections/Navbar/Navbar";
+function App() {
+  return (
+    <div className="App">
+<Navbar/>
+    </div>
+  );
+}
+
+export default App;
