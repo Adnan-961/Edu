127385424ff1ed85f8206ac0536fff7bb8f19740
Adnan-961 <adnan.sayed.official@gmail.com>
2022-09-02 16:32:34 +0300

    format

--- edu/src/App.js
+++ edu/src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./Sections/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
-<Navbar/>
+      <Navbar/>
     </div>
   );
 }
--- edu/src/Sections/Navbar/Navbar.jsx
+++ edu/src/Sections/Navbar/Navbar.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import "./styles.scss"
+import React from "react";
+import "./styles.scss";
 import Logo from "../../assets/images/logo.png";
 export default function Navbar() {
   return (
-    <div className='navbar__container'>
-            <div className="nav__container">
-                <img src={Logo} alt="Logo" />
+    <div className="navbar__container">
+      <div className="nav__container">
+        <img src={Logo} alt="Logo" />
 
-                <div className="navigation">
-                        <div className="nav__link">
-                            <span>Become a student</span>
-                        </div>
-                        <div className="nav__link">
-                            <span>Become a Teacher</span>
-                        </div>
-                        <div className="nav__link">
-                            <span>Login</span>
-                        </div>
-                        <div className="nav__link">
-                            <button>Login</button>
-                        </div>
-                </div>
-            </div>
+        <div className="navigation">
+          <div className="nav__link">
+            <span>Become a student</span>
+          </div>
+          <div className="nav__link">
+            <span>Become a Teacher</span>
+          </div>
+          <div className="nav__link">
+            <span>Login</span>
+          </div>
+          <div className="nav__link">
+            <button>Login</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
