35940f879658452b0b8fbc6ac2ba6cd197ad3686
Adnan-961 <adnan.sayed.official@gmail.com>
2022-09-02 19:11:08 +0300

    Progress

---
+++ edu/src/Components/Course/Course.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Course.scss"
+export default function Course() {
+  return (
+    <div className="course">
+        
+    </div>
+  )
+}
--- edu/src/Sections/OurCourses/OurCourses.jsx
+++ edu/src/Sections/OurCourses/OurCourses.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
-import "./styles.scss"
+import React from "react";
+import "./styles.scss";
+import Course from "../../Components/Course/Course";
 export default function CategoryCards() {
   return (
     <div className="our__courses__section">
-      <div className="courses__title"><span>Our Course</span></div>
-      <div className="courses__subtitle"><span>Show Our Unique Course</span></div>
-        <div className="courses__categories">
-          {["Business","Business","Business","Business","Business","Business","Business"].map((cat)=><span>{cat}</span>)}
-        </div>
+      <div className="courses__title">
+        <span>Our Course</span>
+      </div>
+      <div className="courses__subtitle">
+        <span>Show Our Unique Course</span>
+      </div>
+      <div className="courses__categories">
+        {[
+          "Business",
+          "Design",
+          "Development ",
+          "Language",
+          "Life Style",
+          "Medical Science",
+          "Technology",
+        ].map((cat) => (
+          <span>{cat}</span>
+        ))}
+      </div>
+
+      <div className="courses">
+        <Course />
+        <Course />
+        <Course />
+        <Course />
+        <Course />
+        <Course /> 
+        <Course />
+        <Course />
+        <Course />
+      </div>
     </div>
-  )
+  );
 }
