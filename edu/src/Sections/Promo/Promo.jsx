0163b32f1851c5db243c21ff512e7a816c58735e
Adnan-961 <adnan.sayed.official@gmail.com>
2022-09-05 16:39:45 +0300

    Added features

--- edu/src/Sections/Promo/Promo.jsx
+++ edu/src/Sections/Promo/Promo.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import "./styles.scss"
-import img from "../../assets/images/kids.jpg"
+import React from "react";
+import "./styles.scss";
+import img from "../../assets/images/kids.jpg";
+import check from "../../assets/images/check.png";
+import { data } from "./data";
 export default function Promo() {
   return (
     <div className="promo_container">
-            
-          <div className="img__container">  <img src={img} alt="promo" className='promo_img' /></div>
-          <div className="app__container">
-            <h3>Download apps</h3>
-            <h1>Download Our Mobile App Start Learning Today</h1>
+      <div className="img__container">
+        {" "}
+        <img src={img} alt="promo" className="promo_img" />
+      </div>
+      <div className="app__container">
+        <h3>Download apps</h3>
+        <h1>Download Our Mobile App Start Learning Today</h1>
 
-            <div className="features">
-                features
+        <div className="features">
+          {data.map((feat) => (
+            <div className="feature">
+              <img src={check} alt="check" />
+              <span>{feat.name}</span>
             </div>
+          ))}
+        </div>
 
-            <div className="promo__btns">
-                btn
-            </div>
-          </div>
+        <div className="promo__btns">btn</div>
+      </div>
     </div>
-  )
+  );
 }
---
+++ edu/src/Sections/Promo/data.js
@@ -0,0 +1,14 @@
+export const data = [
+  {
+    name: "Resource Download",
+  },
+  {
+    name: "Offline Access",
+  },
+  {
+    name: "Course Notification",
+  },
+  {
+    name: "Course Notification",
+  },
+];
