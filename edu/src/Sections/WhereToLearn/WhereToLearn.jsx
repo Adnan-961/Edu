8cdf0b879694bcada96b89434403ea308d1bb611
Adnan-961 <adnan.sayed.official@gmail.com>
2022-09-05 17:46:48 +0300

    progress

--- edu/src/Sections/WhereToLearn/WhereToLearn.jsx
+++ edu/src/Sections/WhereToLearn/WhereToLearn.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles.scss";
+import machine from "../../assets/images/machine-learning.png"
 export default function WhereToLearn() {
   return (
     <div className="learn__container">
@@ -12,7 +13,20 @@ export default function WhereToLearn() {
           veritatis, omnis dolorem quia ipsum eum quasi animi magni. Dicta, ab
           unde?
         </p>
-        <div className="learn__features"></div>
+        <div className="learn__features">
+          <div className="learn_feature">
+            <div className="learn_img"><img src={machine} alt="machine" width={"50px"} /></div>
+            <div className="learn_text">
+              <div cp lassName="title">
+                <h3>App-Based Learning</h3>{" "}
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Consectetur dolorum magnam, facilis sunt ipsam officia
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="learn__images"></div>
     </div>
