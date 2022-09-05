b28d7be56cd18ccfaf5f7b051f43dddab387b4a6
Adnan-961 <adnan.sayed.official@gmail.com>
2022-09-05 17:54:22 +0300

    progress

--- edu/src/Sections/WhereToLearn/WhereToLearn.jsx
+++ edu/src/Sections/WhereToLearn/WhereToLearn.jsx
@@ -26,6 +26,26 @@ export default function WhereToLearn() {
               </div>
             </div>
           </div>
+{/*                                                                              */}
+          <div className="learn_feature">
+            <div className="learn_img"><img src={machine} alt="machine" width={"50px"} /></div>
+            <div className="learn_text">
+              <div cp lassName="title">
+                <h3>App-Based Learning</h3>{" "}
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Consectetur dolorum magnam, facilis sunt ipsam officia
+                </p>
+              </div>
+            </div>
+          </div>
+{/*                                                                               */}
+
+            <div className="learn__contact">
+                <div className="btn">
+                    <span>More About</span>
+                </div>
+            </div>
         </div>
       </div>
       <div className="learn__images"></div>
