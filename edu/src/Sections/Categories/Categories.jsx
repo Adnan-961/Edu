4d8b8fe37b76ed94027391a68b7055a29ce507e3
Adnan-961 <adnan.sayed.official@gmail.com>
2022-09-05 15:55:11 +0300

    Added Category section

--- edu/src/Sections/Categories/Categories.jsx
+++ edu/src/Sections/Categories/Categories.jsx
@@ -1,13 +1,65 @@
 import React from "react";
 import "./styles.scss";
 import Category from "../../Components/Category/Category";
-import { langauge } from "./data";
+import { health, langauge } from "./data";
+import { tutoring } from "./data";
+import { music } from "./data";
+import { Sports } from "./data";
+import { Art } from "./data";
+
 export default function Categories() {
   return (
     <div className="category__section">
-        <div className="category__title"><h2>Find a Teacher Anywhere</h2></div>
+      <div className="category__title">
+        <h2>Languages</h2>
+      </div>
       <div className="categories_container">
         {langauge.map((cat) => (
+          <Category name={cat.name} />
+        ))}
+      </div>
+
+      <div className="category__title">
+        <h2>Academic Tutoring</h2>
+      </div>
+      <div className="categories_container">
+        {tutoring.map((cat) => (
+          <Category name={cat.name} />
+        ))}
+      </div>
+
+      <div className="category__title">
+        <h2>Music</h2>
+      </div>
+      <div className="categories_container">
+        {music.map((cat) => (
+          <Category name={cat.name} />
+        ))}
+      </div>
+
+      <div className="category__title">
+        <h2>Gym & Sports</h2>
+      </div>
+      <div className="categories_container">
+        {Sports.map((cat) => (
+          <Category name={cat.name} />
+        ))}
+      </div>
+
+      <div className="category__title">
+        <h2>Art & Craft</h2>
+      </div>
+      <div className="categories_container">
+        {Art.map((cat) => (
+          <Category name={cat.name} />
+        ))}
+      </div>
+
+      <div className="category__title">
+        <h2>Health & Wellness</h2>
+      </div>
+      <div className="categories_container">
+        {health.map((cat) => (
           <Category name={cat.name} />
         ))}
       </div>
--- edu/src/Sections/Categories/data.js
+++ edu/src/Sections/Categories/data.js
@@ -1,29 +1,53 @@
 export const langauge = [
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
-  { name: "yoruba" },
+  { name: "Yoruba" },
+  { name: "French" },
+  { name: "Spanish" },
+  { name: "English" },
+  { name: "Yoruba" },
+  { name: "French" },
+  { name: "Spanish" },
+  { name: "English" },  { name: "Yoruba" },
+  { name: "French" },
+  { name: "Spanish" },
+  { name: "English" },  { name: "Yoruba" },
+  { name: "French" },
+  { name: "Spanish" },
+  { name: "English" },  { name: "Yoruba" },
+  { name: "French" },
+  { name: "Spanish" },
+  { name: "English" },  { name: "Yoruba" },
+  { name: "French" },
+  { name: "Spanish" },
+  { name: "English" },
 ];
+
+export const tutoring = [
+    { name: "Biology" },
+    { name: "Physics" },
+    { name: "Chemistry" },
+    { name: "Public Speaking" },
+  ];
+  
+  export const music = [
+    { name: "Accordion" },
+    { name: "Guitar" },
+    { name: "Sound Engineering"},
+    { name: "Music Production" },
+  ];
+  export const Sports = [
+    { name: "Gym & Sports" },
+    { name: "Karate" },
+    { name: "Judo"},
+    { name: "Taekwondo" },
+  ];
+  export const Art = [
+    { name: "Knitting" },
+    { name: "Photography" },
+    { name: "Videography"},
+    { name: "Sewing Class" },
+  ];
+  export const health = [
+    { name: "Yoga" },
+    { name: "CPR" },
+  ];
+  
