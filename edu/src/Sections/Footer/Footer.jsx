728f8d3276798469da2e00d2830806bd04d53a42
Adnan-961 <adnan.sayed.official@gmail.com>
2022-09-06 12:35:57 +0300

    footer input

--- edu/src/Sections/Footer/Footer.jsx
+++ edu/src/Sections/Footer/Footer.jsx
@@ -1,134 +1,140 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-
-import './Footer.styles.scss';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import EmailIcon from '@mui/icons-material/Email';
-import PhoneIcon from '@mui/icons-material/Phone';
-import whiteLogo from '../../assets/images/logo.png'
-
+import "./Footer.styles.scss";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneIcon from "@mui/icons-material/Phone";
+import whiteLogo from "../../assets/images/logo.png";
+import SendIcon from "@mui/icons-material/Send";
 
 const Column1 = () => {
-    
-    return(
-        <div className='Footer_Column'>
-            <h1>Explore</h1>
-            <a>Home</a>
-            <a 
-                to="/#about"  
-                scroll={(el) => el.scrollIntoView({ behavior: 'auto', block: 'center' })}
-            >
-                About Us
-            </a>
-            <a 
-                to="/#team" 
-                scroll={(el) => el.scrollIntoView({ 
-                    behavior: 'auto', 
-                    block: 'center' 
-                })} 
-            >
-                Become A Teacher
-            </a>
-            <a to="/#contact" >
-                Become A Student 
-            </a>
-            <a 
-                to="/#newsletter" 
-                scroll={(el) => el.scrollIntoView({ 
-                    behavior: 'auto', 
-                    block: 'center' 
-                })} 
-            >
-                Subscribe 
-            </a>
-        </div>
-    )
-}
+  return (
+    <div className="Footer_Column">
+      <h1>Explore</h1>
+      <a>Home</a>
+      <a
+        to="/#about"
+        scroll={(el) =>
+          el.scrollIntoView({ behavior: "auto", block: "center" })
+        }
+      >
+        About Us
+      </a>
+      <a
+        to="/#team"
+        scroll={(el) =>
+          el.scrollIntoView({
+            behavior: "auto",
+            block: "center",
+          })
+        }
+      >
+        Become A Teacher
+      </a>
+      <a to="/#contact">Become A Student</a>
+      <a
+        to="/#newsletter"
+        scroll={(el) =>
+          el.scrollIntoView({
+            behavior: "auto",
+            block: "center",
+          })
+        }
+      >
+        Subscribe
+      </a>
+    </div>
+  );
+};
 
 const Logos = () => {
+  return (
+    <div className="Logos_Container">
+      <div className="title">
+        {/* <img alt="reboost logo" src={whiteLogo} className="Footer_logo" /> */}
+        {/* <h1>Let's connect</h1> */}
+        <h1>
+          Sign Up For a <br /> Newsletter
+        </h1>
+        <p>Offers, promotion and educational contents</p>
+      </div>
 
-  
-    return (
-        <div className='Logos_Container'>
-            <div className='title'>
-                {/* <img alt="reboost logo" src={whiteLogo} className='Footer_logo'/> */}
-                <h1>Let's connect</h1>
-            </div>
-            <div className='Logos' >   
-                <a 
-                    href='https://www.instagram.com/educifyinc/' 
-                    target="_blank" 
-                    rel="noopener noreferrer" 
-                >
-                    <InstagramIcon className='Footer_icons' />
-                </a>
-                <a 
-                    href='https://www.facebook.com/educifyinc' 
-                    target="_blank" 
-                    rel="noopener noreferrer" 
-                >
-                    <FacebookIcon className='Footer_icons'  />
-                </a>
-                <a 
-                    href='https://www.linkedin.com/' 
-                    target="_blank" rel="noopener noreferrer" 
-                >
-                    <LinkedInIcon className='Footer_icons' />
-                </a>
-                <a 
-                    href='https://twitter.com/' 
-                    target="_blank" 
-                    rel="noopener noreferrer" 
-                >
-                    <TwitterIcon className='Footer_icons' />
-                </a>
-            </div>
+      <div className="send__email">
+        <input type="text" placeholder="Email Address" />
+        <div className="email__icon">
+          <SendIcon />
         </div>
-    )
-}
-const Links=["About", "Portfolio", "Contact", "Templates"]
+      </div>
+      {/* <div className="Logos">
+        <a
+          href="https://www.instagram.com/educifyinc/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InstagramIcon className="Footer_icons" />
+        </a>
+        <a
+          href="https://www.facebook.com/educifyinc"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FacebookIcon className="Footer_icons" />
+        </a>
+        <a
+          href="https://www.linkedin.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedInIcon className="Footer_icons" />
+        </a>
+        <a
+          href="https://twitter.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <TwitterIcon className="Footer_icons" />
+        </a>
+      </div> */}
+    </div>
+  );
+};
+const Links = ["About", "Portfolio", "Contact", "Templates"];
 
 function InfoSection() {
-    return (
-        <div className='Info_Container' id='footer'>
-        
-       
-            <Logos />
-            <Column1 title="Info" links={Links}/>
-            <div className='ContactInfo'>
-                <h1>Contact Info</h1>
-                <a href="mailto:bowlakemusic@gmail.com" id="Contact">
-                    <EmailIcon  className='icons'/>
-                    <p className=''>bowlakemusic@gmail.com</p>
-                </a>
-                <a  title="Call SF" href="tel:+96171523153" >
-                    <PhoneIcon  className='icons'/>
-                    <p className=''> +(000)-00-000000</p>
-                </a>
-                <a href="https://api.whatsapp.com/send?phone=000000000">
-                    <WhatsAppIcon className='icons'/>
-                    <p>Contact us on WhatsApp</p>
-                </a>
-            </div>
-         
-        </div>
-    )
+  return (
+    <div className="Info_Container" id="footer">
+      <Logos />
+      <Column1 title="Info" links={Links} />
+      <div className="ContactInfo">
+        <h1>Contact Info</h1>
+        <a href="mailto:bowlakemusic@gmail.com" id="Contact">
+          <EmailIcon className="icons" />
+          <p className="">bowlakemusic@gmail.com</p>
+        </a>
+        <a title="Call SF" href="tel:+96171523153">
+          <PhoneIcon className="icons" />
+          <p className=""> +(000)-00-000000</p>
+        </a>
+        <a href="https://api.whatsapp.com/send?phone=000000000">
+          <WhatsAppIcon className="icons" />
+          <p>Contact us on WhatsApp</p>
+        </a>
+      </div>
+    </div>
+  );
 }
 function Footer() {
-    let year = new Date().getFullYear()
-    return (
-        <div className='Footer_Container' id='footer'>
-            <InfoSection />
-            <p className='Footer_Copyright'>
-                Copyright ©{year} All rights reserved 
-            </p>
-        </div>
-    )
+  let year = new Date().getFullYear();
+  return (
+    <div className="Footer_Container" id="footer">
+      <InfoSection />
+      <p className="Footer_Copyright">Copyright ©{year} All rights reserved</p>
+    </div>
+  );
 }
 
-
-export default Footer
+export default Footer;
--- edu/src/Sections/WhereToLearn/WhereToLearn.jsx
+++ edu/src/Sections/WhereToLearn/WhereToLearn.jsx
@@ -1,44 +1,48 @@
 import React from "react";
 import "./styles.scss";
-import machine from "../../assets/images/machine-learning.png"
-import three from "../../assets/images/three.png"
+import machine from "../../assets/images/machine-learning.png";
+import three from "../../assets/images/three.png";
+import Location from "../../assets/images/location.png";
+import globe from "../../assets/images/globe.png";
 
 let data = [
   {
     title: "Home",
     icon: machine,
-    description: "Do you want your teacher to come to your house for music lessons? Well, wherever you are, we’ll be coming for you.",
+    description:
+      "Do you want your teacher to come to your house for music lessons? Well, wherever you are, we’ll be coming for you.",
   },
   {
     title: "Location",
-    icon: machine,
-    description: "Want to take music lessons at your favorite place? We are on the go! Yes, that’s right! The choice of place is all yours, whether in a cafe, in a library, in our studio or wherever you want, we’ll be there for you."
+    icon: Location,
+    description:
+      "Want to take music lessons at your favorite place? We are on the go! Yes, that’s right! The choice of place is all yours, whether in a cafe, in a library, in our studio or wherever you want, we’ll be there for you.",
   },
   {
     title: "Virtual",
-    icon: machine,
-    description: "Are you up to learning music lessons in our virtual classrooms? Then, meet your music teacher online through your smartphones, laptop or computer."
-  }
-]
+    icon: globe,
+    description:
+      "Are you up to learning music lessons in our virtual classrooms? Then, meet your music teacher online through your smartphones, laptop or computer.",
+  },
+];
 
-const LearningContainer = ({title, description, icon}) => {
-  return(
+const LearningContainer = ({ title, description, icon }) => {
+  return (
     <div className="learn_feature">
-      <img src={icon} alt="machine" className="learn_img"/>
+      <img src={icon} alt="machine" className="learn_img" />
       <div className="learn_text">
         <div cp lassName="title">
           <h3>{title}</h3>
-          <p>
-            {description}
-          </p>
+          <p>{description}</p>
         </div>
       </div>
-  </div>
-  )
-}
+    </div>
+  );
+};
 
 export default function WhereToLearn() {
-  let description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur dolorum magnam, facilis sunt ipsam officia"
+  let description =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur dolorum magnam, facilis sunt ipsam officia";
   return (
     <div className="learn__container">
       <div className="learn__about">
@@ -51,15 +55,21 @@ export default function WhereToLearn() {
           unde?
         </p>
         <div className="learn__features">
-          {
-            data && data.map((item, i) =>  <LearningContainer  title={item.title} description={item.description} icon={item.icon} key={i}/>)
-          }
+          {data &&
+            data.map((item, i) => (
+              <LearningContainer
+                title={item.title}
+                description={item.description}
+                icon={item.icon}
+                key={i}
+              />
+            ))}
           <div className="learn__contact_btn">
             <span>More About</span>
           </div>
         </div>
       </div>
-        <img alt="" src={three} className="learn__images"/>
+      <img alt="" src={three} className="learn__images" />
     </div>
   );
 }
