05a9d539d58f47e66c68790bfd0d4da152a3e4d0
Adnan-961 <adnan.sayed.official@gmail.com>
2022-09-06 16:01:48 +0300

    added progress

--- edu/src/App.js
+++ edu/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Header />
-      {/* <OurCourses /> */}
+      <OurCourses />
       <Promo />
       <WhereToLearn />
       <Categories />
