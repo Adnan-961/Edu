eb54651d633b71a227c1bfee967cc4eb2f99c312
Adnan-961 <adnan.sayed.official@gmail.com>
2022-09-08 15:12:28 +0300

    Progress, header + Categories

--- edu/src/Sections/Categories/Categories.jsx
+++ edu/src/Sections/Categories/Categories.jsx
@@ -1,37 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 import Category from "../../Components/Category/Category";
 import { categories } from "./data";
-
-const CategoryContainer = ({array, title}) => {
-  console.log("array")
-  return(
+import { countries } from "./data";
+const CategoryContainer = ({ array, title }) => {
+  return (
     <div>
       <div className="category__title">
         <h2>{title}</h2>
       </div>
       <div className="categories_container">
-        {
-          array.map((cat, i) => (
-            <div className="category_card" key={i}>
-              {cat}
-            </div>
-          ))
-        }
+        {array.map((cat, i) => (
+          <div className="category_card" key={i}>
+            {cat}
+          </div>
+        ))}
       </div>
     </div>
-
-  )
-}
+  );
+};
 
 export default function Categories() {
+  const [isActiveCountries, setIsActiveCountries] = useState(true);
+
   return (
     <div className="category__section">
-      {
-        categories.map((item, i) => 
-          <CategoryContainer key={i} array={item.array} title={item.name}/>
-        )
-      }
+      <div className="categories__filter">
+        <h4
+          className={isActiveCountries && "active"}
+          onClick={() => setIsActiveCountries(true)}
+        >
+          Find a Teacher Anywhere
+        </h4>
+        <h4
+          className={!isActiveCountries && "active"}
+          onClick={() => setIsActiveCountries(false)}
+        >
+          Trending Private Lessons
+        </h4>
+      </div>
+      {!isActiveCountries &&
+        categories.map((item, i) => (
+          <CategoryContainer key={i} array={item.array} title={item.name} />
+        ))}
+
+      {isActiveCountries &&
+        countries.map((item, i) => (
+          <CategoryContainer key={i} array={item.array} title={item.array[i]} />
+        ))}
     </div>
   );
 }
--- edu/src/Sections/Categories/data.js
+++ edu/src/Sections/Categories/data.js
@@ -1,122 +1,81 @@
-export const langauge = [
-  { name: "Yoruba" },
-  { name: "French" },
-  { name: "Spanish" },
-  { name: "English" },
-  { name: "Yoruba" },
-  { name: "French" },
-  { name: "Spanish" },
-  { name: "English" },  
-  { name: "Yoruba" },
-  { name: "French" },
-  { name: "Spanish" },
-  { name: "English" },  
-  { name: "Yoruba" },
-  { name: "French" },
-  { name: "Spanish" },
-  { name: "English" },  
-  { name: "Yoruba" },
-  { name: "French" },
-  { name: "Spanish" },
-  { name: "English" },  
-  { name: "Yoruba" },
-  { name: "French" },
-  { name: "Spanish" },
-  { name: "English" },
+export const countries = [
+  {
+    array: [
+      "USA",
+      "Canada",
+      "USA",
+      "Canada",
+      "USA",
+      "Canada",
+      "USA",
+      "Canada",
+      "USA",
+      "Canada",
+      "USA",
+      "Canada",
+      "USA",
+      "Canada",
+      "USA",
+      "Canada",
+      "USA",
+      "Canada",
+      "USA",
+      "Canada",
+      "USA",
+      "Canada",
+      "USA",
+      "Canada",
+      "USA",
+      "Canada",
+      "USA",
+      "Canada",
+    ],
+  },
 ];
-
-export const tutoring = [
-    { name: "Biology" },
-    { name: "Physics" },
-    { name: "Chemistry" },
-    { name: "Public Speaking" },
-  ];
-  
-  export const music = [
-    { name: "Accordion" },
-    { name: "Guitar" },
-    { name: "Sound Engineering"},
-    { name: "Music Production" },
-  ];
-  export const Sports = [
-    { name: "Gym & Sports" },
-    { name: "Karate" },
-    { name: "Judo"},
-    { name: "Taekwondo" },
-  ];
-  export const Art = [
-    { name: "Knitting" },
-    { name: "Photography" },
-    { name: "Videography"},
-    { name: "Sewing Class" },
-  ];
-  export const health = [
-    { name: "Yoga" },
-    { name: "CPR" },
-  ];
-  
-  export const categories = [
-    {
-      name: "Languages",
-      array: [
-        "Yoruba",
-        "French",
-        "Spanish",
-        "English",
-        "Yoruba",
-        "French",
-        "Spanish",
-        "English",
-        "Yoruba",
-        "French",
-        "Spanish",
-        "English",
-        "Yoruba",
-        "French",
-        "Spanish",
-        "English",
-        "Yoruba",
-        "French",
-        "Spanish",
-        "English",
-        "Yoruba",
-        "French",
-        "Spanish",
-        "English",
-      ]
-    },
-    {
-      name: "Music",
-      array: [
-        "Accordion",
-        "Guitar",
-        "Sound Engineering",
-        "Music Production"
-      ]
-    },
-    {
-      name: "Sports",
-      array:[
-        "Gym & Sports",
-        "Karate",
-        "Judo",
-        "Taekwondo"
-      ]
-    },
-    {
-      name: "Art & Craft",
-      array: [
-        "Knitting",
-        "Photography",
-        "Videography",
-        "Sewing Class",
-      ]
-    },
-    {
-      name:  "Health & Wellness",
-      array:[
-        "Yoga",
-        "CPR"
-      ]
-    }
-  ]
+export const categories = [
+  {
+    name: "Languages",
+    array: [
+      "Yoruba",
+      "French",
+      "Spanish",
+      "English",
+      "Yoruba",
+      "French",
+      "Spanish",
+      "English",
+      "Yoruba",
+      "French",
+      "Spanish",
+      "English",
+      "Yoruba",
+      "French",
+      "Spanish",
+      "English",
+      "Yoruba",
+      "French",
+      "Spanish",
+      "English",
+      "Yoruba",
+      "French",
+      "Spanish",
+      "English",
+    ],
+  },
+  {
+    name: "Music",
+    array: ["Accordion", "Guitar", "Sound Engineering", "Music Production"],
+  },
+  {
+    name: "Sports",
+    array: ["Gym & Sports", "Karate", "Judo", "Taekwondo"],
+  },
+  {
+    name: "Art & Craft",
+    array: ["Knitting", "Photography", "Videography", "Sewing Class"],
+  },
+  {
+    name: "Health & Wellness",
+    array: ["Yoga", "CPR"],
+  },
+];
