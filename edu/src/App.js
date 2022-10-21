ee8f12929ef316e35e8dbd92bc67abd87391c5c7
Adnan-961 <adnan.sayed.official@gmail.com>
2022-10-21 17:57:19 +0300

    progress

--- edu/src/App.js
+++ edu/src/App.js
@@ -17,11 +17,11 @@ function App() {
       <Navbar />
       <Header />
       {/* <OurCourses /> */}
-      {/* <RelatedLessons /> */}
+      <RelatedLessons />
       <WhereToLearn />
-      <Categories />
-      <Promo />
-      <Newsletter />
+      {/* <Categories /> */}
+      {/* <Promo /> */}
+      {/* <Newsletter /> */}
       <Footer />
       {/* <Info /> */}
     </div>
